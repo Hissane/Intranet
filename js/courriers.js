7f7bc380ca3a07d48e7b6e7b1bc180463a8393c8
assalielmehdi@gmail.com <assalielmehdi@gmail.com>
2019-07-27 15:23:18 +0100

    Redesign and add js file for infos

--- js/courriers.js
+++ js/courriers.js
@@ -1,30 +1,37 @@
 const courriers = [
   {
-    ref:  5180,
-    src: "Ministère des Affaires Étrangères et de la Coopération Internationale",
+    ref: 5180,
+    src:
+      "Ministère des Affaires Étrangères et de la Coopération Internationale",
     obj: "Reponse a la demande de bourses aux etudiants senegalais"
   },
   {
-    ref:  5181,
-    src: "Ministère de la Fonction Publique et de la Modernisation de l'Administration",
+    ref: 5181,
+    src:
+      "Ministère de la Fonction Publique et de la Modernisation de l'Administration",
     obj: "Nouvelle reforme concernan les employes de la DCP"
   },
   {
-    ref:  5182,
-    src: "Ministre de l'Enseignement Supérieur, de la Recherche Scientifique et de la Formation des Cadres",
-    obj: "RNote relative aux dossiers des étudiants, lauréats, enseignants et administratifs des établissements d'enseignement supérieur privé"
+    ref: 5182,
+    src:
+      "Ministre de l'Enseignement Supérieur, de la Recherche Scientifique et de la Formation des Cadres",
+    obj:
+      "RNote relative aux dossiers des étudiants, lauréats, enseignants et administratifs des établissements d'enseignement supérieur privé"
   }
 ];
 
-const makeCourrier = ({ref, src, obj}) => (
+const makeCourrier = ({ ref, src, obj }) =>
   `<tr>
     <th scope="row"></th>
     <td>${ref}</td>
     <td>${src}</td>
     <td>${obj}</td>
-    <td><i class="fas fa-file-pdf"></i></td>
-  </tr>`
-);
+    <td class="text-center">
+      <button class="icon-btn">
+        <i class="fas fa-file-pdf"></i>
+      </button>
+    </td>
+  </tr>`;
 
 $(window).on("load", () => {
   const courriersTableBody = $("#courriers");
---
+++ js/info_actu.js
@@ -0,0 +1,47 @@
+const infos = [
+  {
+    title: "Info 1",
+    body:
+      "Proin suscipit ipsum vel lacus suscipit iaculis. Morbi lacinia justo at consequat molestie. Vestibulum vulputate volutpat ante et lobortis. Proin luctus euismod urna, ut vehicula ex finibus at. Nam a augue eget diam egestas tincidunt. Aliquam vel ullamcorper nulla, id facilisis libero. Mauris sed nisi a eros sollicitudin varius.",
+    img: "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+  },
+  {
+    title: "Info 2",
+    body:
+      "Proin suscipit ipsum vel lacus suscipit iaculis. Morbi lacinia justo at consequat molestie. Vestibulum vulputate volutpat ante et lobortis. Proin luctus euismod urna, ut vehicula ex finibus at. Nam a augue eget diam egestas tincidunt. Aliquam vel ullamcorper nulla, id facilisis libero. Mauris sed nisi a eros sollicitudin varius."
+  },
+  {
+    title: "Info 3",
+    body:
+      "Proin suscipit ipsum vel lacus suscipit iaculis. Morbi lacinia justo at consequat molestie. Vestibulum vulputate volutpat ante et lobortis. Proin luctus euismod urna, ut vehicula ex finibus at. Nam a augue eget diam egestas tincidunt. Aliquam vel ullamcorper nulla, id facilisis libero. Mauris sed nisi a eros sollicitudin varius.",
+    img: "https://www.newton.ac.uk/files/covers/968361.jpg"
+  },
+  {
+    title: "Info 4",
+    body:
+      "Proin suscipit ipsum vel lacus suscipit iaculis. Morbi lacinia justo at consequat molestie. Vestibulum vulputate volutpat ante et lobortis. Proin luctus euismod urna, ut vehicula ex finibus at. Nam a augue eget diam egestas tincidunt. Aliquam vel ullamcorper nulla, id facilisis libero. Mauris sed nisi a eros sollicitudin varius."
+  }
+];
+
+const makeInfoCard = ({ title, body, img }) => {
+  const imgTag = `<img src="${img}" class="card-img-top" />`;
+  return `<div class="card">
+      ${img ? imgTag : ""}
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${body.substring(0, 80) +
+          (body.length > 80 ? "..." : "")}</p>
+        <p class="card-text text-right">
+          <button class="btn btn-outline-secondary">
+            <i class="fab fa-readme mr-2"></i>
+            Read more
+          </button>
+        </p>
+      </div>
+    </div>`;
+};
+
+$(window).on("load", () => {
+  const infosCards = $("#infos");
+  infos.map(makeInfoCard).forEach(card => infosCards.append(card));
+});

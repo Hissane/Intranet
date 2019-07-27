98084a99a5736ea16c2a6011307355864fc8b8f6
assalielmehdi@gmail.com <assalielmehdi@gmail.com>
2019-07-27 15:49:18 +0100

    Add events to calendar dynamically

--- js/evenements.js
+++ js/evenements.js
@@ -1,34 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
+const events = [
+  {
+    title: "Event 1",
+    start: "2019-07-09T00:00:00",
+    allDay: true
+  },
+  {
+    title: "Event 2",
+    start: "2019-07-15T00:00:00",
+    allDay: true
+  },
+  {
+    title: "Event 3",
+    start: "2019-07-29T00:00:00",
+    allDay: true
+  }
+];
 
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'dayGrid' ],
-    defaultView: 'dayGridMonth',
+$(window).on("load", () => {
+  const calendarEl = document.getElementById("calendar");
+
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: ["dayGrid"],
+    defaultView: "dayGridMonth",
     header: {
-      center: 'addEventButton'
+      center: "addEventButton"
     },
     customButtons: {
       addEventButton: {
-        text: 'add event',
+        text: "add event",
         click: function() {
-          var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-          var titleevent = prompt('Enter event title');
-          var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+          var dateStr = prompt("Enter a date in YYYY-MM-DD format");
+          var titleevent = prompt("Enter event title");
+          var date = new Date(dateStr + "T00:00:00");
 
-          if (!isNaN(date.valueOf())) { // valid?
+          if (!isNaN(date.valueOf())) {
             calendar.addEvent({
               title: titleevent,
               start: date,
               allDay: true
             });
-            alert('Great. Now, update your database...');
+            alert("Great. Now, update your database...");
           } else {
-            alert('Invalid date.');
+            alert("Invalid date.");
           }
         }
       }
     }
   });
+
+  events.forEach(event => calendar.addEvent(event));
 
   calendar.render();
 });
